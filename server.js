8ae81e9aaedd90d2457382dc2b0f988a1ab956c5
vmshalkey <vmshalkey@gmail.com>
2015-10-28 11:30:38 -0700

    installed mysql and connected db

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express')
 var app = express()
 app.listen(8000);
 
+
 // Sockets
 // io = require('socket.io').listen(server)
 
@@ -25,3 +26,26 @@ require('./server/config/mongoose.js');
 require('./server/config/routes.js')(app);
 // Socket Routes
 // require('./server/config/socket/routes.js')(app);
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+	host: "localhost",
+	user: "root",
+	password: "root",
+	database: "lighthouse"
+});
+
+con.connect(function(err){
+	if(err){
+		console.log('Error connecting to database');
+		return;
+	}
+	console.log('Connection to MySQL DB established');
+});
+
+con.end(function(err) {
+	// The connection is terminated gracefully
+	// Ensures all previously enqueued queries are still
+	// before sending a COM_QUIT packet to the MySQL server.
+});
